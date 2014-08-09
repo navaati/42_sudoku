559a61f38453cdf3c0499eb2cbdb79cb2c4a6e06
Léo Gillot-Lamure <leo.gillot@navaati.net>
2014-08-09 23:10:05 +0200

    simplification des types

--- ex00/types.h
+++ ex00/types.h
@@ -6,19 +6,16 @@
 /*   By: lgillot- <lgillot-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/08/09 17:41:39 by lgillot-          #+#    #+#             */
-/*   Updated: 2014/08/09 18:12:55 by lgillot-         ###   ########.fr       */
+/*   Updated: 2014/08/09 22:04:45 by lgillot-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITFIELD_H
 # define BITFIELD_H
-typedef struct
-{
-	int	is_alone;
-	union
-	{
-		char alone;
-		char bits;
-	}
-}	t_case;
+
+# include <stdint.h>
+
+typedef uint16_t	t_case;
+typedef t_case		t_sudoku[9][9];
+
 #endif
