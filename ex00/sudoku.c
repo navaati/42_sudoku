26894a9760dcee481d9a4e1d353b566b267b8113
Léo Gillot-Lamure <leo.gillot@navaati.net>
2014-08-10 02:07:15 +0200

    translating shit

--- ex00/sudoku.c
+++ ex00/sudoku.c
@@ -6,41 +6,41 @@
 /*   By: lgillot- <lgillot-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/08/09 22:07:26 by lgillot-          #+#    #+#             */
-/*   Updated: 2014/08/10 01:46:34 by lgillot-         ###   ########.fr       */
+/*   Updated: 2014/08/10 02:04:58 by lgillot-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-t_case	*trouve_case_vide(t_sudoku *sudoku)
+t_case	*next_empty_cell(t_sudoku *sudoku)
 {
 	// boucle while
 }
 
 int 	sudoku(t_sudoku *sudoku)
 {
-	t_case	*case_vide;
-	t_case	possibilités;
-	t_case	possibilité_courante;
+	t_case	*empty_cell;
+	t_case	possible_vals;
+	t_case	curr_val;
 	sudoku	try;
 	
-	case_vide = trouve_case_vide(&sudoku);
-	/* deux cas de base */
-	if (case_vide == NULL) // On est au bout du tableau, le sudoku est complet
+	empty_cell = next_empty_cell(&sudoku);
+	/* two base case */
+	if (empty_cell == NULL) // We're at end of array, sudoku is complete
 		return (1);
-	possibilités = trouve_possibilités(t_case, sudoku);
-	if (possibilités == 0) // Ou alors on ne trouve aucune possibilité, on fail 
+	possible_vals = lookup_possibilities(empty_cell, sudoku);
+	if (possible_vals == 0) // Or there is no possible value for this cell, we fail
 	{
 		return (-1);
 	}
 	
-	/* cas de récursion */
-	pour_chaque_possibilité(possibilité_courante, possibilités)
+	/* recursion case : we fill the cell with an unsure value and we recurse */
+	for_each_possibility(curr_val, possible_vals)
 	{
-		*case_vide = possibilité_courante;
+		*empty_cell = current_val;
 		try = *sudoku;
-		if (sudoku(&try) == 1)
+		if (sudoku(&try) == 1) // If the recursive-us succeeds...
 		{
 			*sudoku = try;
-			return(1);
+			return(1); // ...we were right and we're done
 		}
 	}
 	return(-1);
